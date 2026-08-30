acfdcfa2b6af03350eeb759ecd83b13352ffc7fb
thigrecom <thigre.com@gmail.com>
2026-08-30 18:16:17 -0300

    Update products.js

--- products.js
+++ products.js
@@ -86,8 +86,7 @@ const PRODUCTS_DATA = {
     images: [
       "https://raw.githubusercontent.com/thigrecom/1/refs/heads/main/img/mudas1.jpeg",
       "https://raw.githubusercontent.com/thigrecom/1/refs/heads/main/img/mudas2.jpeg",
-      "https://raw.githubusercontent.com/thigrecom/1/refs/heads/main/img/mudas3.jpeg",
-      "https://raw.githubusercontent.com/thigrecom/1/refs/heads/main/img/mudas4.jpeg"
+      "https://raw.githubusercontent.com/thigrecom/1/refs/heads/main/img/mudas3.jpeg"
     ],
     variants: [
       { name: "Muda de Babosa", price: 12.00, available: true },
