d234f27ec880be436f15c60a9d277838b47e9b6b
thigrecom <thigre.com@gmail.com>
2026-08-30 14:36:00 -0300

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const PRODUCTS_DATA = {
     name: "Cacau 100%",
     tag: "# intenso",
     images: [
-      "./img/barras1.jpg",
+      "./1/barras1.jpg",
       "https://images.unsplash.com/photo-1606312619070-d48b4c652a52?auto=format&fit=crop&w=600&q=80",
       "https://images.unsplash.com/photo-1511381939415-e44015466834?auto=format&fit=crop&w=600&q=80"
     ],
